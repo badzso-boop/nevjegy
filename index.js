9dd43141b6b0523d9395e309e9c3c5495f0770e2
Badzso <norbert.ujj@gmail.com>
2023-06-06 23:15:50 +0200

    hupsz

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
-const mysql = require("mysql2");
+const mysql2 = require("mysql2");
 const path = require("path");
 const nodemailer = require("nodemailer");
 
